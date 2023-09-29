b1292d8b9a3b8510036c0c18414c25edfad94736
Gabriel Santos <108304564+GabrielFelipeS@users.noreply.github.com>
2023-09-29 14:15:44 -0300

    Bug em usuario, commit ignorado

--- inc/modelo/Usuario.php
+++ inc/modelo/Usuario.php
@@ -1,8 +1,5 @@
 <?php
 class Usuario{
-    private $conn;
-
-    
     private $nome;
     private $nascimento;
     private $telefone;
@@ -10,10 +7,15 @@ class Usuario{
     private $senha;
     private $fotoPerfil;
 
-    function __construct($conn){
-        $this->conn = $conn;
-        
+    function __construct($nome, $nascimento, $telefone, $email, $senha, $fotoPerfil){
+        $this->nome = $nome;
+        $this->nascimento = $nascimento;
+        $this->telefone = $telefone;
+        $this->email = $email;
+        $this->senha = $senha;
+        $this->fotoPerfil = $fotoPerfil;
     }
+    
     function get_nome(){
         return $this->nome;
     }
@@ -50,39 +52,5 @@ class Usuario{
     //function set_fotoPerfil($fotoPerfil){
     //    $this->fotoPerfil = $fotoPerfil; 
     //}
-    public function verificarEmailExistente($email) {
-        $sql = "SELECT COUNT(*) as count FROM usuario WHERE email = ?";
-        
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-        // Verifique se o e-mail já existe
-        return $row['count'] > 0;
-    }
-
-    function cadastrar($nome, $nascimento, $telefone, $email, $senha) {
-        $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-        $dataSQL = date('Y-m-d', strtotime(str_replace('/', '-', $nascimento)));
-        //var_dump($_FILES);
-        $ext = strtolower(substr($_FILES['fotoPerfil']['name'],-4)); //Pegando extensão do arquivo
-        $new_name =  $nome.$ext; //Definindo um novo nome para o arquivo
-        $dir = '../fotosPerfil/'; //Diretório para uploads, coloquei em lib pra facilitar o senhor achar
-        move_uploaded_file($_FILES['fotoPerfil']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo 
-        $caminho = 'fotosPerfil/'.$new_name;
-
-        $sql = "INSERT INTO usuario (nome, nascimento, telefone, email, senha, fotoPerfil) VALUES 
-            (?, ?, ?, ?, ?, ?)";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bind_param("ssssss", $nome, $dataSQL, $telefone, $email, $senhaHash, $caminho);
-        
-        if ($stmt->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
 ?>
